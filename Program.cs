50b6b76986ffc3cef6f85bdd366de476ba6e3090
Eliseu da Silva Rodrigues <eliseu.rodrigues@onlineapp.com.br>
2020-11-04 17:54:55 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_10_Calcula_Poupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula poupança");
+
+            double valorInvestido = 1000;
+            int mesInvestido = 1;
+
+
+
+            while (mesInvestido <= 12)
+            {
+
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+
+                Console.WriteLine("mes " + mesInvestido + " gerou um total de R$" + valorInvestido);
+
+                mesInvestido++;
+
+
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
